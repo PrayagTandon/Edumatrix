a39fb394874aed46423785cee2120cf2129da976
Prayag Tandon <tandonprayag99@gmail.com>
2024-04-01 13:21:31 +0530

    Setting up the project environment

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <p className="text-3xl font-medium text-sky-800">Hello World</p>;
+  return (
+    <div>
+      <p className="text-3xl font-medium text-sky-800">Hello World</p>
+      <Button variant={"destructive"}>Click Me</Button>
+    </div>
+  );
 }
